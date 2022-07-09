c1bb621839c19fb72da4e919082600385b3fe439
ShimpeiHashizume <69233878+ShimpeiHashizume@users.noreply.github.com>
2022-07-09 12:11:28 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import "./styles.css";
+
+// 追加ボタンを押したときの関数
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.querySelector("#add-text").value;
+  document.querySelector("#add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  // spanタグ生成
+  const span = document.createElement("span");
+  span.textContent = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // li以下を初期化
+    addTarget.textContent = null;
+
+    // spanタグ生成
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    // buttonタグ(戻す)生成
+    const backButton = document.createElement("button");
+    backButton.textContent = "戻す";
+
+    // liタグの子要素に各要素を設定
+    addTarget.appendChild(span);
+    addTarget.appendChild(backButton);
+
+    // liタグを完了リストに追加
+    document.querySelector("#complete-list").appendChild(addTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(span);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.querySelector("#incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.querySelector("#incomplete-list").removeChild(target);
+};
+
+// 追加ボタンを押したときの実行
+document
+  .querySelector("#add-button")
+  .addEventListener("click", () => onClickAdd());
