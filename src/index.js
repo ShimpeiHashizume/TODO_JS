1aa3f42a005651857b042c868cf3ca5fa588f300
ShimpeiHashizume <69233878+ShimpeiHashizume@users.noreply.github.com>
2022-07-09 13:33:03 +0900

    機能追加（追加、削除、戻す）

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.querySelector("#add-text").value;
   document.querySelector("#add-text").value = "";
+  createIncompleteList(inputText);
+};
 
+// 戻すボタンを押したときの関数
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.querySelector("#incomplete-list").removeChild(target);
+};
+
+//　未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // liタグ生成
   const li = document.createElement("li");
   li.className = "list-row";
 
   // spanタグ生成
   const span = document.createElement("span");
-  span.textContent = inputText;
+  span.textContent = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -36,6 +47,15 @@ const onClickAdd = () => {
     // buttonタグ(戻す)生成
     const backButton = document.createElement("button");
     backButton.textContent = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(li)を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.querySelector("#complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerHTML;
+      createIncompleteList(text);
+    });
 
     // liタグの子要素に各要素を設定
     addTarget.appendChild(span);
@@ -60,11 +80,6 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.querySelector("#incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.querySelector("#incomplete-list").removeChild(target);
 };
 
 // 追加ボタンを押したときの実行
